657bfae7c3e39ff344ee0cb0dc07f26e52275ee2
boncis <wendigotech@gmail.com>
2018-10-08 23:10:48 +0200

    fix modals

--- page-portfolio.php
+++ page-portfolio.php
@@ -26,17 +26,37 @@ get_header(); ?>
                 <?php $portfolio_item_number = 0; ?>
                 <?php while ( $portfolio->have_posts() && $portfolio_item_number++ < 1 ) : $portfolio->the_post(); ?>
                     <div class="card " style="grid-area:one;" id="one">
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal', array(
-                                'class' => 'card-img-top'
-                            ) );
-                            }
-                         ?>
-                        <div class="card-body">
-                            <h4 class="card-title"><?php the_title(); ?></h4>
-                            <?php the_excerpt( ); ?>
-                            <a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-primary" data-toggle="modal" data-target="#modal1"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        <div class="modal fade pg-show-modal" id="modal1" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="true"> 
+                            <div class="modal-dialog modal-lg"> 
+                                <div class="modal-content"> 
+                                    <div class="modal-header"> 
+                                        <h4 class="modal-title"><?php the_title(); ?></h4> 
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>                                                         
+                                    </div>                                                     
+                                    <div class="modal-body"> 
+                                        <?php the_content(); ?> 
+                                    </div>                                                     
+                                    <div class="modal-footer"> 
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">
+                                            <?php _e( 'Close', 'blog' ); ?>
+                                        </button>                                                         
+                                    </div>                                                     
+                                </div>                                                 
+                            </div>                                             
+                        </div>
+                        <div id="card-inner">
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal', array(
+                                    'class' => 'card-img-top'
+                                ) );
+                                }
+                             ?>
+                            <div class="card-body">
+                                <h4 class="card-title"><?php the_title(); ?></h4>
+                                <?php the_excerpt( ); ?>
+                                <a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-dark" data-toggle="modal" data-target="#modal1"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                            </div>
                         </div>
                     </div>
                 <?php endwhile; ?>
@@ -44,24 +64,6 @@ get_header(); ?>
             <?php else : ?>
                 <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
             <?php endif; ?>
-            <div class="modal fade pg-show-modal" id="modal1" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static"> 
-                <div class="modal-dialog modal-lg"> 
-                    <div class="modal-content"> 
-                        <div class="modal-header"> 
-                            <h4 class="modal-title"><?php the_title(); ?></h4> 
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>                                             
-                        </div>                                         
-                        <div class="modal-body"> 
-                            <?php the_content(); ?> 
-                        </div>                                         
-                        <div class="modal-footer"> 
-                            <button type="button" class="btn btn-default" data-dismiss="modal">
-                                <?php _e( 'Close', 'blog' ); ?>
-                            </button>                                             
-                        </div>                                         
-                    </div>                                     
-                </div>                                 
-            </div>
             <?php if ( $portfolio->have_posts() ) : ?>
                 <?php $portfolio_item_number = 0; ?>
                 <?php while ( $portfolio->have_posts() && $portfolio_item_number++ < 1 ) : $portfolio->the_post(); ?>
@@ -84,17 +86,19 @@ get_header(); ?>
                                 </div>                                                 
                             </div>                                             
                         </div>
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal', array(
-                                'class' => 'card-img-top'
-                            ) );
-                            }
-                         ?>
-                        <div class="card-body">
-                            <h4 class="card-title"><?php the_title(); ?></h4>
-                            <?php the_excerpt( ); ?>
-                            <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modal2"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        <div id="card-inner">
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal', array(
+                                    'class' => 'card-img-top'
+                                ) );
+                                }
+                             ?>
+                            <div class="card-body">
+                                <h4 class="card-title"><?php the_title(); ?></h4>
+                                <?php the_excerpt( ); ?>
+                                <a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-dark" data-toggle="modal" data-target="#modal1"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                            </div>
                         </div>
                     </div>
                 <?php endwhile; ?>
@@ -106,17 +110,37 @@ get_header(); ?>
                 <?php $portfolio_item_number = 0; ?>
                 <?php while ( $portfolio->have_posts() && $portfolio_item_number++ < 1 ) : $portfolio->the_post(); ?>
                     <div class="card " style="grid-area:three;">
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal', array(
-                                'class' => 'card-img-top'
-                            ) );
-                            }
-                         ?>
-                        <div class="card-body">
-                            <h4 class="card-title"><?php the_title(); ?></h4>
-                            <?php the_excerpt( ); ?>
-                            <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modal3"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        <div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="true"> 
+                            <div class="modal-dialog modal-lg"> 
+                                <div class="modal-content"> 
+                                    <div class="modal-header"> 
+                                        <h4 class="modal-title"><?php the_title(); ?></h4> 
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>                                                         
+                                    </div>                                                     
+                                    <div class="modal-body"> 
+                                        <?php the_content(); ?> 
+                                    </div>                                                     
+                                    <div class="modal-footer"> 
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">
+                                            <?php _e( 'Close', 'blog' ); ?>
+                                        </button>                                                         
+                                    </div>                                                     
+                                </div>                                                 
+                            </div>                                             
+                        </div>
+                        <div id="card-inner">
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal', array(
+                                    'class' => 'card-img-top'
+                                ) );
+                                }
+                             ?>
+                            <div class="card-body">
+                                <h4 class="card-title"><?php the_title(); ?></h4>
+                                <?php the_excerpt( ); ?>
+                                <a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-dark" data-toggle="modal" data-target="#modal1"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                            </div>
                         </div>
                     </div>
                 <?php endwhile; ?>
@@ -124,24 +148,6 @@ get_header(); ?>
             <?php else : ?>
                 <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
             <?php endif; ?>
-            <div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="true"> 
-                <div class="modal-dialog modal-lg"> 
-                    <div class="modal-content"> 
-                        <div class="modal-header"> 
-                            <h4 class="modal-title"><?php the_title(); ?></h4> 
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>                                             
-                        </div>                                         
-                        <div class="modal-body"> 
-                            <?php the_content(); ?> 
-                        </div>                                         
-                        <div class="modal-footer"> 
-                            <button type="button" class="btn btn-default" data-dismiss="modal">
-                                <?php _e( 'Close', 'blog' ); ?>
-                            </button>                                             
-                        </div>                                         
-                    </div>                                     
-                </div>                                 
-            </div>
             <?php
                 $portfolio_args = array(
                     'category_name' => 'portfolio'
@@ -169,17 +175,19 @@ get_header(); ?>
                                 </div>                                                 
                             </div>                                             
                         </div>
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal', array(
-                                'class' => 'card-img-top'
-                            ) );
-                            }
-                         ?>
-                        <div class="card-body">
-                            <h4 class="card-title"><?php the_title(); ?></h4>
-                            <?php the_excerpt( ); ?>
-                            <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        <div id="card-inner">
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal', array(
+                                    'class' => 'card-img-top'
+                                ) );
+                                }
+                             ?>
+                            <div class="card-body">
+                                <h4 class="card-title"><?php the_title(); ?></h4>
+                                <?php the_excerpt( ); ?>
+                                <a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-dark" data-toggle="modal" data-target="#modal1"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                            </div>
                         </div>
                     </div>
                 <?php endwhile; ?>
@@ -209,17 +217,19 @@ get_header(); ?>
                                 </div>                                                 
                             </div>                                             
                         </div>
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal', array(
-                                'class' => 'card-img-top'
-                            ) );
-                            }
-                         ?>
-                        <div class="card-body">
-                            <h4 class="card-title"><?php the_title(); ?></h4>
-                            <?php the_excerpt( ); ?>
-                            <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modal4"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        <div id="card-inner">
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal', array(
+                                    'class' => 'card-img-top'
+                                ) );
+                                }
+                             ?>
+                            <div class="card-body">
+                                <h4 class="card-title"><?php the_title(); ?></h4>
+                                <?php the_excerpt( ); ?>
+                                <a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-dark" data-toggle="modal" data-target="#modal1"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                            </div>
                         </div>
                     </div>
                 <?php endwhile; ?>
@@ -249,17 +259,19 @@ get_header(); ?>
                                 </div>                                                 
                             </div>                                             
                         </div>
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal', array(
-                                'class' => 'card-img-top'
-                            ) );
-                            }
-                         ?>
-                        <div class="card-body">
-                            <h4 class="card-title"><?php the_title(); ?></h4>
-                            <?php the_excerpt( ); ?>
-                            <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modal6"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        <div id="card-inner">
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal', array(
+                                    'class' => 'card-img-top'
+                                ) );
+                                }
+                             ?>
+                            <div class="card-body">
+                                <h4 class="card-title"><?php the_title(); ?></h4>
+                                <?php the_excerpt( ); ?>
+                                <a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-dark" data-toggle="modal" data-target="#modal1"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                            </div>
                         </div>
                     </div>
                 <?php endwhile; ?>
