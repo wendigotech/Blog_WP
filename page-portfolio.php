cc01184babd7012354148caf0dd5e51fe08d2d7e
boncis <wendigotech@gmail.com>
2018-10-08 20:27:26 +0200

    4

--- page-portfolio.php
+++ page-portfolio.php
@@ -14,30 +14,6 @@ get_header(); ?>
                 <?php the_excerpt( ); ?> 
             </div>
         </div>
-        <div class="modal fade pg-show-modal" id="modal1" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="true"> 
-            <div class="modal-dialog modal-lg"> 
-                <div class="modal-content"> 
-                    <div class="modal-header"> 
-                        <h4 class="modal-title"><?php _e( 'Modal title', 'blog' ); ?></h4> 
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>                                         
-                    </div>                                     
-
-                    <div class="modal-body"> 
-                        <p><?php _e( 'One fine body&hellip;', 'blog' ); ?></p> 
-                    </div>                                     
-
-                    <div class="modal-footer"> 
-                        <button type="button" class="btn btn-default" data-dismiss="modal">
-                            <?php _e( 'Close', 'blog' ); ?>
-                        </button>                                         
-
-                        <button type="button" class="btn btn-primary">
-                            <?php _e( 'Save changes', 'blog' ); ?>
-                        </button>                                         
-                    </div>                                     
-                </div>                                 
-            </div>                             
-        </div>
         <?php
             $portfolio_args = array(
                 'category_name' => 'portfolio',
@@ -46,9 +22,33 @@ get_header(); ?>
         ?>
         <?php $portfolio = new WP_Query( $portfolio_args ); ?>
         <?php if ( $portfolio->have_posts() ) : ?>
-            <div class="row portfolio-grid">
-                <?php while ( $portfolio->have_posts() ) : $portfolio->the_post(); ?>
+            <?php while ( $portfolio->have_posts() ) : $portfolio->the_post(); ?>
+                <div class="row portfolio-grid">
                     <div class="card " style="grid-area:one;" id="1">
+                        <div class="modal fade pg-show-modal" id="modal1" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="true"> 
+                            <div class="modal-dialog modal-lg"> 
+                                <div class="modal-content"> 
+                                    <div class="modal-header"> 
+                                        <h4 class="modal-title"><?php _e( 'Modal title', 'blog' ); ?></h4> 
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>                                                         
+                                    </div>                                                     
+
+                                    <div class="modal-body"> 
+                                        <p><?php _e( 'One fine body&hellip;', 'blog' ); ?></p> 
+                                    </div>                                                     
+
+                                    <div class="modal-footer"> 
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">
+                                            <?php _e( 'Close', 'blog' ); ?>
+                                        </button>                                                         
+
+                                        <button type="button" class="btn btn-primary">
+                                            <?php _e( 'Save changes', 'blog' ); ?>
+                                        </button>                                                         
+                                    </div>                                                     
+                                </div>                                                 
+                            </div>                                             
+                        </div>
                         <?php if ( is_singular() ) : ?>
                             <?php
                                 if ( has_post_thumbnail() ) {
@@ -68,41 +68,55 @@ get_header(); ?>
                             <a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-primary" data-toggle="modal" data-target="#modal1"><?php _e( 'Go somewhere', 'blog' ); ?></a>
                         </div>
                     </div>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-                <div class="card " style="grid-area:three;">
-                    <img class="card-img-top" alt="Card image cap" src="http://pinegrow.com/placeholders/img19.jpg"/>
-                    <div class="card-body">
-                        <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4>
-                        <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
-                        <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                    <div class="card " style="grid-area:two;" id="2">
+                        <?php
+                            if ( has_post_thumbnail() ) {
+                                the_post_thumbnail( 'normal', array(
+                                'class' => 'card-img-top'
+                            ) );
+                            }
+                         ?>
+                        <div class="card-body">
+                            <h4 class="card-title"><?php the_title(); ?></h4>
+                            <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
+                            <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        </div>
                     </div>
+                    <div class="card " style="grid-area:three;">
+                        <img class="card-img-top" alt="Card image cap" src="http://pinegrow.com/placeholders/img19.jpg"/>
+                        <div class="card-body">
+                            <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4>
+                            <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
+                            <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        </div>
+                    </div>
+                    <div class="card " style="grid-area:six;">
+                        <img class="card-img-top" alt="Card image cap" src="http://pinegrow.com/placeholders/img19.jpg"/>
+                        <div class="card-body">
+                            <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4>
+                            <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
+                            <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        </div>
+                    </div>
+                    <div class="card " style="grid-area:four;">
+                        <img class="card-img-top" alt="Card image cap" src="http://pinegrow.com/placeholders/img19.jpg"/>
+                        <div class="card-body">
+                            <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4>
+                            <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
+                            <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        </div>
+                    </div>
+                    <div class="card " style="grid-area:five;">
+                        <img class="card-img-top" alt="Card image cap" src="http://pinegrow.com/placeholders/img19.jpg"/>
+                        <div class="card-body">
+                            <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4>
+                            <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
+                            <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
+                        </div>
+                    </div>                                     
                 </div>
-                <div class="card " style="grid-area:six;">
-                    <img class="card-img-top" alt="Card image cap" src="http://pinegrow.com/placeholders/img19.jpg"/>
-                    <div class="card-body">
-                        <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4>
-                        <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
-                        <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
-                    </div>
-                </div>
-                <div class="card " style="grid-area:four;">
-                    <img class="card-img-top" alt="Card image cap" src="http://pinegrow.com/placeholders/img19.jpg"/>
-                    <div class="card-body">
-                        <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4>
-                        <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
-                        <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
-                    </div>
-                </div>
-                <div class="card " style="grid-area:five;">
-                    <img class="card-img-top" alt="Card image cap" src="http://pinegrow.com/placeholders/img19.jpg"/>
-                    <div class="card-body">
-                        <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4>
-                        <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p>
-                        <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a>
-                    </div>
-                </div>                                 
-            </div>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
         <?php else : ?>
             <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
         <?php endif; ?> 
