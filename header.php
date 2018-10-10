8dcb76efb1c7d3486b4da87c49056f792585e257
boncis <wendigotech@gmail.com>
2018-10-10 13:48:31 +0200

    add google tag manager

--- header.php
+++ header.php
@@ -10,9 +10,21 @@
         <meta name="description" content="<?php bloginfo( 'description' ); ?>"> 
         <meta name="author" content="The Pinegrow Team">          
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">                                     
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-526CT5W');</script>
+        <!-- End Google Tag Manager -->
         <?php wp_head(); ?>
     </head>     
-    <body class="<?php echo implode(' ', get_body_class()); ?>"> 
+    <body class="<?php echo implode(' ', get_body_class()); ?>">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-526CT5W" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <div class="hfeed site" id="page"> 
             <header> 
                 <div itemscope="" itemtype="http://schema.org/WebSite" id="wrapper-navbar"> 
