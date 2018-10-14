ddb2db7bc29ef59a3e76cd410ff20209c5a8ad94
boncis <wendigotech@gmail.com>
2018-10-14 14:00:36 +0200

    22

--- page-portfolio.php
+++ page-portfolio.php
@@ -16,16 +16,6 @@ get_header(); ?>
         </div>
         <div class="row no-gutters port-grid">
             <div class="p-4 col-lg-12">
-                <div class="mb-4">
-                    <button class="btn js-toggle-grid-gap">
-                        <?php _e( 'toggle', 'blog' ); ?> 
-                        <code><?php _e( 'grid-gap', 'blog' ); ?></code>
-                    </button>
-                    <button class="btn js-toggle-grid-columns">
-                        <?php _e( 'toggle', 'blog' ); ?> 
-                        <code><?php _e( 'grid-template-columns', 'blog' ); ?></code>
-                    </button>
-                </div>
                 <?php
                     $portfolio_args = array(
                         'category_name' => 'portfolio',
@@ -36,10 +26,16 @@ get_header(); ?>
                 <?php if ( $portfolio->have_posts() ) : ?>
                     <div <?php post_class( 'grid mb-4' ); ?> id="post-<?php the_ID(); ?>">
                         <?php while ( $portfolio->have_posts() ) : $portfolio->the_post(); ?>
-                            <div class="card" style="grid-area:1 / 1 / 4 / 3;" id="1"> 
-                                <img class="card-img-top" src="http://pinegrow.com/placeholders/img15.jpg" alt="Card image cap"> 
+                            <div class="card" style="grid-area: 1 / 1 / 3 / 3; justify-content: center;" id="1"> 
+                                <?php
+                                    if ( has_post_thumbnail() ) {
+                                        the_post_thumbnail( 'normal', array(
+                                        'class' => 'card-img-top'
+                                    ) );
+                                    }
+                                 ?> 
                                 <div class="card-body"> 
-                                    <h4 class="card-title"><?php _e( 'Card title', 'blog' ); ?></h4> 
+                                    <h4 class="card-title"><?php the_title(); ?></h4> 
                                     <h6 class="card-subtitle mb-2 text-muted"><?php _e( 'Card subtitle', 'blog' ); ?></h6> 
                                     <p class="card-text"><?php _e( 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', 'blog' ); ?></p> 
                                     <a href="#" class="btn btn-primary"><?php _e( 'Go somewhere', 'blog' ); ?></a> 
