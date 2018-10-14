16ae087029a17376210c8d558c8b40c9cd9f9e2f
boncis <wendigotech@gmail.com>
2018-10-14 22:01:34 +0200

    36

--- page-portfolio.php
+++ page-portfolio.php
@@ -15,7 +15,7 @@ get_header(); ?>
             </div>
         </div>                         
         <div class="row mt-5 mb-5">
-            <section class="cards" style="display:grid;grid-template-columns:20px 1fr 1fr 1fr 20px;grid-template-rows:100px 100px 1fr 100px 100px 1fr 100px 100px;grid-gap:20px;">
+            <section class="cards cards-grid">
                 <?php
                     $portfolio_args = array(
                         'category_name' => 'portfolio',
@@ -69,6 +69,48 @@ get_header(); ?>
                     <?php $portfolio_item_number = 0; ?>
                     <?php while ( $portfolio->have_posts() && $portfolio_item_number++ < 1 ) : $portfolio->the_post(); ?>
                         <article class="card card--1" style="grid-area:3 / 4 / 6 / 5;">
+                            <?php $image_attributes = (is_singular() || in_the_loop()) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal', 'with-image' ) : null; ?>
+                            <div class="card__img <?php if($image_attributes) echo 'with-image'; ?>" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"></div>
+                            <a href="#" class="card_link" data-toggle="modal" data-target="#modal1">
+                                <?php $image_attributes = (is_singular() || in_the_loop()) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal', 'with-image' ) : null; ?>
+                                <div class="card__img--hover <?php if($image_attributes) echo 'with-image'; ?>" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"></div>
+                            </a>
+                            <div class="card__info">
+                                <span class="card__category"><?php single_tag_title(); ?></span>
+                                <h3 class="card__title"><?php the_title(); ?></h3>
+                                <span class="card__by"><?php _e( 'by', 'blog' ); ?> <a href="#" class="card__author" title="author"><?php the_author(); ?></a></span>
+                            </div>
+                        </article>
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+                <?php else : ?>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
+                <?php endif; ?>
+                <?php if ( $portfolio->have_posts() ) : ?>
+                    <?php $portfolio_item_number = 0; ?>
+                    <?php while ( $portfolio->have_posts() && $portfolio_item_number++ < 1 ) : $portfolio->the_post(); ?>
+                        <article class="card card--1" style="grid-area:1 / 5 / 4 / 6;">
+                            <?php $image_attributes = (is_singular() || in_the_loop()) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal', 'with-image' ) : null; ?>
+                            <div class="card__img <?php if($image_attributes) echo 'with-image'; ?>" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"></div>
+                            <a href="#" class="card_link" data-toggle="modal" data-target="#modal1">
+                                <?php $image_attributes = (is_singular() || in_the_loop()) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal', 'with-image' ) : null; ?>
+                                <div class="card__img--hover <?php if($image_attributes) echo 'with-image'; ?>" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"></div>
+                            </a>
+                            <div class="card__info">
+                                <span class="card__category"><?php single_tag_title(); ?></span>
+                                <h3 class="card__title"><?php the_title(); ?></h3>
+                                <span class="card__by"><?php _e( 'by', 'blog' ); ?> <a href="#" class="card__author" title="author"><?php the_author(); ?></a></span>
+                            </div>
+                        </article>
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+                <?php else : ?>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
+                <?php endif; ?>
+                <?php if ( $portfolio->have_posts() ) : ?>
+                    <?php $portfolio_item_number = 0; ?>
+                    <?php while ( $portfolio->have_posts() && $portfolio_item_number++ < 1 ) : $portfolio->the_post(); ?>
+                        <article class="card card--1" style="grid-area:4 / 5 / 7 / 6;">
                             <?php $image_attributes = (is_singular() || in_the_loop()) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal', 'with-image' ) : null; ?>
                             <div class="card__img <?php if($image_attributes) echo 'with-image'; ?>" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"></div>
                             <a href="#" class="card_link" data-toggle="modal" data-target="#modal1">
