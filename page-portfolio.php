bc3da358a13dc7742205ee4816ee9d106ed5772b
boncis <wendigotech@gmail.com>
2018-10-14 20:27:46 +0200

    30

--- page-portfolio.php
+++ page-portfolio.php
@@ -37,10 +37,11 @@ get_header(); ?>
                                     <span class="card__time"><?php _e( '15 min', 'blog' ); ?></span>
                                 </div>
                             </div>
-                            <?php $image_attributes = (is_singular() || in_the_loop()) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal', 'card_img' ) : null; ?>
-                            <div class="card__img <?php if($image_attributes) echo 'card_img'; ?>" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"></div>
+                            <?php $image_attributes = (is_singular() || in_the_loop()) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal', 'with-image' ) : null; ?>
+                            <div class="card__img <?php if($image_attributes) echo 'with-image'; ?>" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"></div>
                             <a href="#" class="card_link">
-                                <div class="card__img--hover"></div>
+                                <?php $image_attributes = (is_singular() || in_the_loop()) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal', 'with-image' ) : null; ?>
+                                <div class="card__img--hover <?php if($image_attributes) echo 'with-image'; ?>" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"></div>
                             </a>
                             <div class="card__info">
                                 <span class="card__category"><?php single_tag_title(); ?></span>
@@ -51,7 +52,7 @@ get_header(); ?>
                         </article>
                     <?php endwhile; ?>
                     <?php wp_reset_postdata(); ?>
-                    <article class="card card--2 shake-top">
+                    <article class="card card--2">
                         <div class="card__info-hover">
                             <svg class="card__like" viewbox="0 0 24 24">
                                 <path fill="#000000" d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z"/>
