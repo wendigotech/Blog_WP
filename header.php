9c47caa72126e09f7c1fbab8c52845cded4533bb
boncis <wendigotech@gmail.com>
2018-10-15 15:13:25 +0200

    white navbar

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             <header> 
                 <div itemscope="" itemtype="http://schema.org/WebSite" id="wrapper-navbar"> 
                     <a class="skip-link screen-reader-text sr-only" href="#content"><?php _e( 'Skip to content', 'blog' ); ?></a> 
-                    <nav class="navbar navbar-dark navbar-expand-lg bg-dark"> 
+                    <nav class="navbar navbar-expand-lg navbar-light bg-white"> 
                         <div class="container"> 
                             <?php if ( ! has_custom_logo() ) : ?>
                                 <div> 
