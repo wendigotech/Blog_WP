7df31e61c79478303fda68248ad9580ec5dfa0a6
boncis <wendigotech@gmail.com>
2018-10-18 23:57:27 +0200

    55

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             <header> 
                 <div itemscope="" itemtype="http://schema.org/WebSite" id="wrapper-navbar"> 
                     <a class="skip-link screen-reader-text sr-only" href="#content"><?php _e( 'Skip to content', 'blog' ); ?></a> 
-                    <nav class="navbar-expand-lg border-top-0 border-right-0 border-left-0 border border-dark navbar-light navbar bg-light" style="border-bottom-left-radius: 30px; border-bottom-right-radius: 30px; margin-bottom: -20px;"> 
+                    <nav class="navbar-expand-lg border-top-0 border-right-0 border-left-0 border border-dark navbar-light navbar bg-light" style="border-bottom-left-radius: 30px; border-bottom-right-radius: 30px; margin-bottom: -20px; background: linear-gradient(299deg, rgb(26, 41, 128) 5.49451%, rgb(38, 208, 206) 72.5275%);"> 
                         <div class="container"> 
                             <?php if ( ! has_custom_logo() ) : ?>
                                 <div> 
