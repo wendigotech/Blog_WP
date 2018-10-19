c0e1764a36d7ad14eb5d10129ba9cf5db849024b
boncis <wendigotech@gmail.com>
2018-10-19 21:38:11 +0200

    60

--- front-page.php
+++ front-page.php
@@ -4,13 +4,13 @@ get_header(); ?>
 <div class="wrapper" id="index-wrapper"> 
     <div id="content" tabindex="-1" class="container thetop"> 
         <div class="row d-flex flex-row align-content-center">                              
-            <div class="content-area col-md-10" id="primary"> 
+            <div class="content-area col-md-9" id="primary"> 
                 <main class="site-main" id="main"> 
                     <?php get_template_part( 'loop-templates/content' ); ?> 
                 </main>                                 
             </div>                                                          
             <?php if ( get_theme_mod( 'show_right_sidebar' ) ) : ?>
-                <div class="widget-area col-md-2" role="complementary" id="right-sidebar"> 
+                <div class="widget-area col-md-3" role="complementary" id="right-sidebar"> 
                     <?php if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
                         <?php dynamic_sidebar( 'right-sidebar' ); ?>
                     <?php endif; ?>                                                                           
