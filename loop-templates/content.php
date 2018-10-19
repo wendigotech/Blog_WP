e819a2ca4912ee47d5b7a60dc2399d4e81f80c6f
boncis <wendigotech@gmail.com>
2018-10-19 23:02:29 +0200

    63

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -2,25 +2,25 @@
     <?php if ( have_posts() ) : ?>
         <?php while ( have_posts() ) : the_post(); ?>
             <div <?php post_class( 'row flex-row-reverse justify-content-center' ); ?> id="post-<?php the_ID(); ?>">
-                <?php
-                    $Top_args = array(
-                        'tag' => 'top',
-                        'order' => 'DESC'
-                    )
-                ?>
-                <?php $Top = new WP_Query( $Top_args ); ?>
-                <?php if ( $Top->have_posts() ) : ?>
-                    <div id="carousel1" class="carousel slide carousel-fade col-lg-12" data-ride="carousel" data-interval="8000" data-pause="hover"> 
-                        <ol class="carousel-indicators"> 
-                            <li data-target="#carousel1" data-slide-to="0" class="active"></li>                             
+                <div id="carousel1" class="carousel slide carousel-fade col-lg-12" data-ride="carousel" data-interval="8000" data-pause="hover"> 
+                    <ol class="carousel-indicators"> 
+                        <li data-target="#carousel1" data-slide-to="0" class="active"></li>                         
 
-                            <li data-target="#carousel1" data-slide-to="1"></li>                             
+                        <li data-target="#carousel1" data-slide-to="1"></li>                         
 
-                            <li data-target="#carousel1" data-slide-to="2"></li>                             
-                        </ol>                         
+                        <li data-target="#carousel1" data-slide-to="2"></li>                         
+                    </ol>                     
 
-                        <?php while ( $Top->have_posts() ) : $Top->the_post(); ?>
-                            <div class="carousel-inner"> 
+                    <?php
+                        $Top_args = array(
+                            'tag' => 'top',
+                            'order' => 'DESC'
+                        )
+                    ?>
+                    <?php $Top = new WP_Query( $Top_args ); ?>
+                    <?php if ( $Top->have_posts() ) : ?>
+                        <div class="carousel-inner"> 
+                            <?php while ( $Top->have_posts() ) : $Top->the_post(); ?>
                                 <div class="carousel-item active"> 
                                     <?php
                                         if ( has_post_thumbnail() ) {
@@ -44,21 +44,21 @@
                                 </div>                                 
 
                                 <div class="carousel-item"> 
-                                    <img class="d-block w-100" src="http://pinegrow.com/placeholders/img18.jpg" alt="Third slide"> 
+                                    <img class="w-100 d-block" src="http://pinegrow.com/placeholders/img18.jpg" alt="Third slide"> 
                                     <div class="carousel-caption d-none d-md-block"> 
                                         <h3><?php _e( 'Slide label 3', 'blog' ); ?></h3> 
                                         <p><?php _e( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'blog' ); ?></p> 
                                     </div>                                     
-                                </div>                                 
-                            </div>
-                        <?php endwhile; ?>
-                        <?php wp_reset_postdata(); ?> 
-                        <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Previous', 'blog' ); ?></span> </a> 
-                        <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Next', 'blog' ); ?></span> </a> 
-                    </div>
-                <?php else : ?>
-                    <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
-                <?php endif; ?>
+                                </div>
+                            <?php endwhile; ?>
+                            <?php wp_reset_postdata(); ?> 
+                        </div>
+                    <?php else : ?>
+                        <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
+                    <?php endif; ?> 
+                    <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Previous', 'blog' ); ?></span> </a> 
+                    <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Next', 'blog' ); ?></span> </a> 
+                </div>
                 <article class="col-md-5"> 
                     <header class="entry-header"> 
                         <?php if ( is_singular() ) : ?>
