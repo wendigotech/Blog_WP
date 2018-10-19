5d1693a348297d881fb2ccca2a895e2b810cf3c9
boncis <wendigotech@gmail.com>
2018-10-19 23:11:43 +0200

    67

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -1,7 +1,15 @@
 <div class="d-flex flex-column align-items-center justify-content-center"> 
     <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-            <div <?php post_class( 'row flex-row-reverse justify-content-center' ); ?> id="post-<?php the_ID(); ?>">
+        <div <?php post_class( 'row flex-row-reverse justify-content-center' ); ?> id="post-<?php the_ID(); ?>">
+            <div id="carousel1" class="carousel slide carousel-fade col-lg-12" data-ride="carousel" data-interval="8000" data-pause="hover"> 
+                <ol class="carousel-indicators"> 
+                    <li data-target="#carousel1" data-slide-to="0" class="active"></li>                     
+
+                    <li data-target="#carousel1" data-slide-to="1"></li>                     
+
+                    <li data-target="#carousel1" data-slide-to="2"></li>                     
+                </ol>                 
+
                 <?php
                     $Top_args = array(
                         'tag' => 'top',
@@ -10,32 +18,9 @@
                 ?>
                 <?php $Top = new WP_Query( $Top_args ); ?>
                 <?php if ( $Top->have_posts() ) : ?>
-                    <div id="carousel1" class="carousel slide carousel-fade col-lg-12" data-ride="carousel" data-interval="8000" data-pause="hover"> 
-                        <ol class="carousel-indicators"> 
-                            <li data-target="#carousel1" data-slide-to="0" class="active"></li>                             
+                    <?php while ( $Top->have_posts() ) : $Top->the_post(); ?>
+                        <div class="carousel-inner">                              
 
-                            <li data-target="#carousel1" data-slide-to="1"></li>                             
-
-                            <li data-target="#carousel1" data-slide-to="2"></li>                             
-                        </ol>                         
-
-                        <div class="carousel-inner"> 
-                            <?php while ( $Top->have_posts() ) : $Top->the_post(); ?>
-                                <div class="carousel-item active"> 
-                                    <?php
-                                        if ( has_post_thumbnail() ) {
-                                            the_post_thumbnail( 'normal', array(
-                                            'class' => 'd-block w-100'
-                                        ) );
-                                        }
-                                     ?> 
-                                    <div class="carousel-caption d-none d-md-block"> 
-                                        <h3><?php the_title(); ?></h3> 
-                                        <?php the_excerpt( ); ?> 
-                                    </div>                                     
-                                </div>
-                            <?php endwhile; ?>
-                            <?php wp_reset_postdata(); ?> 
                             <div class="carousel-item"> 
                                 <img class="d-block w-100" src="http://pinegrow.com/placeholders/img13.jpg" alt="Second slide"> 
                                 <div class="carousel-caption d-none d-md-block"> 
@@ -45,20 +30,28 @@
                             </div>                             
 
                             <div class="carousel-item"> 
-                                <img class="w-100 d-block" src="http://pinegrow.com/placeholders/img18.jpg" alt="Third slide"> 
+                                <?php
+                                    if ( has_post_thumbnail() ) {
+                                        the_post_thumbnail( 'normal', array(
+                                        'class' => 'w-100 d-block'
+                                    ) );
+                                    }
+                                 ?> 
                                 <div class="carousel-caption d-none d-md-block"> 
-                                    <h3><?php _e( 'Slide label 3', 'blog' ); ?></h3> 
-                                    <p><?php _e( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'blog' ); ?></p> 
+                                    <h3><?php the_title(); ?></h3> 
+                                    <?php the_excerpt( ); ?> 
                                 </div>                                 
                             </div>                             
-                        </div>                         
-
-                        <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Previous', 'blog' ); ?></span> </a> 
-                        <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Next', 'blog' ); ?></span> </a> 
-                    </div>
+                        </div>
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
                 <?php else : ?>
                     <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
-                <?php endif; ?>
+                <?php endif; ?> 
+                <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Previous', 'blog' ); ?></span> </a> 
+                <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Next', 'blog' ); ?></span> </a> 
+            </div>
+            <?php while ( have_posts() ) : the_post(); ?>
                 <article class="col-md-5"> 
                     <header class="entry-header"> 
                         <?php if ( is_singular() ) : ?>
@@ -88,8 +81,8 @@
                     </div>                     
                     <footer class="entry-footer"></footer>                     
                 </article>
-            </div>
-        <?php endwhile; ?>
+            <?php endwhile; ?>
+        </div>
     <?php else : ?>
         <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
     <?php endif; ?> 
