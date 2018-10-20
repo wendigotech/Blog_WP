d151a441ee67496b4dc0a20f9c4bfaa30fa7d3ab
boncis <wendigotech@gmail.com>
2018-10-20 11:48:48 +0200

    add alaska font add jumbotron

--- front-page.php
+++ front-page.php
@@ -3,19 +3,19 @@ get_header(); ?>
 
 <div class="wrapper" id="index-wrapper"> 
     <div id="content" tabindex="-1" class="container thetop"> 
-        <div class="row d-flex flex-row align-content-center">                              
+        <div class="row d-flex flex-row align-content-center"> 
             <div class="content-area col-md-9" id="primary"> 
                 <main class="site-main" id="main"> 
                     <?php get_template_part( 'loop-templates/content' ); ?> 
                 </main>                                 
-            </div>                                                          
+            </div>
             <?php if ( get_theme_mod( 'show_right_sidebar' ) ) : ?>
                 <div class="widget-area col-md-3" role="complementary" id="right-sidebar"> 
                     <?php if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
                         <?php dynamic_sidebar( 'right-sidebar' ); ?>
                     <?php endif; ?>                                                                           
                 </div>
-            <?php endif; ?>
+            <?php endif; ?> 
         </div>                         
     </div>                     
 </div>                 
--- loop-templates/content.php
+++ loop-templates/content.php
@@ -1,48 +1,4 @@
 <div class="d-flex flex-column align-items-center justify-content-center"> 
-    <?php
-        $Top_args = array(
-            'tag' => 'top',
-            'order' => 'DESC'
-        )
-    ?>
-    <?php $Top = new WP_Query( $Top_args ); ?>
-    <?php if ( $Top->have_posts() ) : ?>
-        <div id="carousel1" class="carousel slide" data-ride="carousel"> 
-            <ol class="carousel-indicators"> 
-                <li data-target="#carousel1" data-slide-to="0" class="active"></li>                 
-
-                <li data-target="#carousel1" data-slide-to="1"></li>                 
-
-                <li data-target="#carousel1" data-slide-to="2"></li>                 
-            </ol>             
-
-            <div class="carousel-inner"> 
-                <?php $Top_item_number = 0; ?>
-                <?php while ( $Top->have_posts() ) : $Top->the_post(); ?>
-                    <div class="carousel-item<?php if( $Top_item_number == 0) echo ' active'; ?>"> 
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'normal', array(
-                                'class' => 'd-block w-100'
-                            ) );
-                            }
-                         ?> 
-                        <div class="carousel-caption d-none d-md-block"> 
-                            <h3><?php the_title(); ?></h3> 
-                            <?php the_excerpt( ); ?> 
-                        </div>                         
-                    </div>
-                    <?php $Top_item_number++; ?>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>                                   
-            </div>             
-
-            <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Previous', 'blog' ); ?></span> </a> 
-            <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Next', 'blog' ); ?></span> </a> 
-        </div>
-    <?php else : ?>
-        <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
-    <?php endif; ?>
     <?php if ( have_posts() ) : ?>
         <div <?php post_class( 'row flex-row-reverse justify-content-center' ); ?> id="post-<?php the_ID(); ?>">
             <?php while ( have_posts() ) : the_post(); ?>
@@ -71,7 +27,7 @@
                             <p class="text-center"><?php _e( '&nbsp;by', 'blog' ); ?> <span class="bg-dark"><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"><?php the_author(); ?></a></span></p>
                             <p class="text-center"><span><?php the_modified_date(); ?></span></p>
                             <a class="btn btn-secondary mt-2" href="<?php echo esc_url( get_permalink() ); ?>"><?php _e( 'Read More', 'blog' ); ?></a> 
-                        </div>                                                  
+                        </div>                         
                     </div>                     
                     <footer class="entry-footer"></footer>                     
                 </article>
