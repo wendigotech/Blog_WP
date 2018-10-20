86b0a7ff7767526f65a270d41883dcafe2cbebc4
boncis <wendigotech@gmail.com>
2018-10-20 12:50:28 +0200

    70

--- front-page.php
+++ front-page.php
@@ -1,6 +1,53 @@
 <?php
 get_header(); ?>
 
+<div class="jumbotron jumbotron-2"> 
+    <div class="row"></div>                                                                                                         
+
+    <div class="row">
+        <h1 class="display-3 col-lg-4"><?php _e( 'Hello, world!', 'blog' ); ?><p class="lead"><?php _e( 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.', 'blog' ); ?></p></h1>
+        <?php
+            $Top_args = array(
+                'tag' => 'top',
+                'order' => 'DESC'
+            )
+        ?>
+        <?php $Top = new WP_Query( $Top_args ); ?>
+        <?php if ( $Top->have_posts() ) : ?>
+            <div id="carousel1" class="carousel slide col-lg-8" data-ride="carousel"> 
+                <ol class="carousel-indicators"> 
+                    <li data-target="#carousel1" data-slide-to="0" class="active"></li>                                     
+                    <li data-target="#carousel1" data-slide-to="1"></li>                                     
+                    <li data-target="#carousel1" data-slide-to="2"></li>                                     
+                </ol>                                 
+                <div class="carousel-inner"> 
+                    <?php $Top_item_number = 0; ?>
+                    <?php while ( $Top->have_posts() ) : $Top->the_post(); ?>
+                        <div class="carousel-item<?php if( $Top_item_number == 0) echo ' active'; ?>"> 
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal', array(
+                                    'class' => 'd-block w-100'
+                                ) );
+                                }
+                             ?> 
+                            <div class="carousel-caption d-none d-md-block"> 
+                                <h3><?php the_title(); ?></h3> 
+                                <?php the_excerpt( ); ?> 
+                            </div>                                             
+                        </div>
+                        <?php $Top_item_number++; ?>
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>                                                                           
+                </div>                                 
+                <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Previous', 'blog' ); ?></span> </a> 
+                <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Next', 'blog' ); ?></span> </a> 
+            </div>
+        <?php else : ?>
+            <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
+        <?php endif; ?>
+    </div>
+</div>
 <div class="wrapper" id="index-wrapper"> 
     <div id="content" tabindex="-1" class="container thetop"> 
         <div class="row d-flex flex-row align-content-center"> 
