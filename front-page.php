9735103d7a91df3def558b30d1b231e46a3fb69f
boncis <wendigotech@gmail.com>
2018-10-20 14:48:39 +0200

    81

--- front-page.php
+++ front-page.php
@@ -2,51 +2,51 @@
 get_header(); ?>
 
 <div class="jumbotron jumbotron-2">                      
-    <div class="row d-flex align-items-start justify-content-center flex-row flex-wrap">
+    <div class="row d-flex flex-wrap no-gutters justify-content-center align-items-start align-content-center">
         <div class="d-flex justify-content-center align-items-end flex-column col-md-5 col-lg-2">                              
 
             <h1 class="display-4 bg-light"><?php _e( 'Hello, world!', 'blog' ); ?></h1>
         </div>
-        <div class="col-md-5 col-lg-5">                              
+        <div class="col-md-5 col-lg-6">                              
 
             <p class="lead bg-dark text-light"><?php _e( 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.', 'blog' ); ?></p>
+            <?php
+                $Top_args = array(
+                    'tag' => 'top',
+                    'order' => 'DESC'
+                )
+            ?>
+            <?php $Top = new WP_Query( $Top_args ); ?>
+            <?php if ( $Top->have_posts() ) : ?>
+                <div id="carousel1" class="carousel slide carousel-fade col-sm-12 col-md-10 col-lg-12" data-ride="carousel" data-pause="hover"> 
+                    <ol class="carousel-indicators"> 
+                        <li data-target="#carousel1" data-slide-to="0" class="active"></li>                                         
+                        <li data-target="#carousel1" data-slide-to="1"></li>                                         
+                        <li data-target="#carousel1" data-slide-to="2"></li>                                         
+                    </ol>                                     
+                    <div class="carousel-inner"> 
+                        <?php $Top_item_number = 0; ?>
+                        <?php while ( $Top->have_posts() ) : $Top->the_post(); ?>
+                            <div class="carousel-item<?php if( $Top_item_number == 0) echo ' active'; ?>"> 
+                                <a href="<?php echo esc_url( get_permalink() ); ?>"> <?php the_post_thumbnail( 'normal', array(
+                                            'class' => 'd-block w-100'
+                                    ) ); ?> </a> 
+                                <div class="carousel-caption d-flex d-md-flex"> 
+                                    <h2><?php the_title(); ?></h2> 
+                                    <?php the_excerpt( ); ?> 
+                                </div>                                                 
+                            </div>
+                            <?php $Top_item_number++; ?>
+                        <?php endwhile; ?>
+                        <?php wp_reset_postdata(); ?>                                                                                   
+                    </div>                                     
+                    <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Previous', 'blog' ); ?></span> </a> 
+                    <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Next', 'blog' ); ?></span> </a> 
+                </div>
+            <?php else : ?>
+                <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
+            <?php endif; ?>
         </div>
-        <?php
-            $Top_args = array(
-                'tag' => 'top',
-                'order' => 'DESC'
-            )
-        ?>
-        <?php $Top = new WP_Query( $Top_args ); ?>
-        <?php if ( $Top->have_posts() ) : ?>
-            <div id="carousel1" class="carousel slide carousel-fade col-sm-12 col-md-10 col-lg-8" data-ride="carousel" data-pause="hover"> 
-                <ol class="carousel-indicators"> 
-                    <li data-target="#carousel1" data-slide-to="0" class="active"></li>                                     
-                    <li data-target="#carousel1" data-slide-to="1"></li>                                     
-                    <li data-target="#carousel1" data-slide-to="2"></li>                                     
-                </ol>                                 
-                <div class="carousel-inner"> 
-                    <?php $Top_item_number = 0; ?>
-                    <?php while ( $Top->have_posts() ) : $Top->the_post(); ?>
-                        <div class="carousel-item<?php if( $Top_item_number == 0) echo ' active'; ?>"> 
-                            <a href="<?php echo esc_url( get_permalink() ); ?>"> <?php the_post_thumbnail( 'normal', array(
-                                        'class' => 'd-block w-100'
-                                ) ); ?> </a> 
-                            <div class="carousel-caption d-flex d-md-flex"> 
-                                <h2><?php the_title(); ?></h2> 
-                                <?php the_excerpt( ); ?> 
-                            </div>                                             
-                        </div>
-                        <?php $Top_item_number++; ?>
-                    <?php endwhile; ?>
-                    <?php wp_reset_postdata(); ?>                                                                           
-                </div>                                 
-                <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Previous', 'blog' ); ?></span> </a> 
-                <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only"><?php _e( 'Next', 'blog' ); ?></span> </a> 
-            </div>
-        <?php else : ?>
-            <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
-        <?php endif; ?>
     </div>
 </div>
 <div class="wrapper" id="index-wrapper"> 
