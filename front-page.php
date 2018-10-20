04350908459efdd1f4ab01a9906672db02d72290
boncis <wendigotech@gmail.com>
2018-10-20 14:29:03 +0200

    78

--- front-page.php
+++ front-page.php
@@ -2,12 +2,12 @@
 get_header(); ?>
 
 <div class="jumbotron jumbotron-2">                      
-    <div class="row d-flex justify-content-center">
-        <div class="col-md-2">                              
+    <div class="row d-flex justify-content-center align-items-start">
+        <div class="d-flex justify-content-center align-items-end flex-column col-md-5 col-lg-3">                              
 
             <h1 class="display-4 bg-light"><?php _e( 'Hello, world!', 'blog' ); ?></h1>
         </div>
-        <div class="col-md-3">                              
+        <div class="col-md-5 col-lg-3">                              
 
             <p class="lead bg-dark text-light"><?php _e( 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.', 'blog' ); ?></p>
         </div>
@@ -19,7 +19,7 @@ get_header(); ?>
         ?>
         <?php $Top = new WP_Query( $Top_args ); ?>
         <?php if ( $Top->have_posts() ) : ?>
-            <div id="carousel1" class="carousel slide carousel-fade col-lg-7" data-ride="carousel" data-pause="hover"> 
+            <div id="carousel1" class="carousel slide carousel-fade col-sm-12 col-md-10 col-lg-6" data-ride="carousel" data-pause="hover"> 
                 <ol class="carousel-indicators"> 
                     <li data-target="#carousel1" data-slide-to="0" class="active"></li>                                     
                     <li data-target="#carousel1" data-slide-to="1"></li>                                     
@@ -32,7 +32,7 @@ get_header(); ?>
                             <a href="<?php echo esc_url( get_permalink() ); ?>"> <?php the_post_thumbnail( 'normal', array(
                                         'class' => 'd-block w-100'
                                 ) ); ?> </a> 
-                            <div class="carousel-caption d-none d-md-block"> 
+                            <div class="carousel-caption d-flex d-md-flex"> 
                                 <h2><?php the_title(); ?></h2> 
                                 <?php the_excerpt( ); ?> 
                             </div>                                             
