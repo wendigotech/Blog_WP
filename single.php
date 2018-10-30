02bde8ae96b69a6130128e7427d4229e73b2d875
MB <wendigotech@gmail.com>
2018-10-30 20:57:17 +0100

    111

--- single.php
+++ single.php
@@ -15,51 +15,49 @@ get_header(); ?>
     <div id="content" tabindex="-1" class="container thetop"> 
         <div class="row d-flex flex-row align-content-center"> 
             <div class="content-area col-md-9" id="primary"> 
-                <main id="main"> 
-                    <?php if ( have_posts() ) : ?>
-                        <?php while ( have_posts() ) : the_post(); ?>
-                            <div <?php post_class( 'd-flex flex-column align-items-center justify-content-center' ); ?> id="post-<?php the_ID(); ?>"> 
-                                <?php
-                                    $blog_args = array(
-                                        'category_name' => 'blog',
-                                        'order' => 'DESC'
-                                    )
-                                ?>
-                                <?php $blog = new WP_Query( $blog_args ); ?>
-                                <?php if ( $blog->have_posts() ) : ?>
-                                    <?php while ( $blog->have_posts() ) : $blog->the_post(); ?>
-                                        <div <?php post_class( 'row site-main' ); ?> id="post-<?php the_ID(); ?>">
-                                            <div class="col-md-12"> 
-                                                <?php
-                                                    if ( has_post_thumbnail() ) {
-                                                        the_post_thumbnail( 'normal', array(
-                                                        'class' => 'w-100 img-thumbnail'
-                                                    ) );
-                                                    }
-                                                 ?> 
-                                            </div>
-                                            <div class="col-md-12">
-                                                <h3><?php the_title(); ?></h3> 
-                                                <?php the_content(); ?> 
-                                            </div>
+                <?php if ( have_posts() ) : ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <div <?php post_class( 'd-flex flex-column align-items-center justify-content-center' ); ?> id="post-<?php the_ID(); ?>"> 
+                            <?php
+                                $blog_args = array(
+                                    'category_name' => 'blog',
+                                    'order' => 'DESC'
+                                )
+                            ?>
+                            <?php $blog = new WP_Query( $blog_args ); ?>
+                            <?php if ( $blog->have_posts() ) : ?>
+                                <?php while ( $blog->have_posts() ) : $blog->the_post(); ?>
+                                    <div <?php post_class( 'row site-main' ); ?> id="post-<?php the_ID(); ?>">
+                                        <div class="col-md-12 mt-3"> 
+                                            <?php
+                                                if ( has_post_thumbnail() ) {
+                                                    the_post_thumbnail( 'normal', array(
+                                                    'class' => 'w-100'
+                                                ) );
+                                                }
+                                             ?> 
                                         </div>
-                                    <?php endwhile; ?>
-                                    <?php wp_reset_postdata(); ?>
-                                <?php else : ?>
-                                    <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
-                                <?php endif; ?> 
-                                <nav aria-label="Posts navigation"> 
-                                    <?php posts_nav_link( null, __( '&#xAB; Newer Posts', 'blog' ), __( 'Older Posts &#xBB;', 'blog' ) ); ?> 
-                                </nav>                                                 
-                            </div>
-                        <?php endwhile; ?>
-                    <?php else : ?>
-                        <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
-                    <?php endif; ?> 
-                </main>                                 
+                                        <div class="col-md-12 mt-5">
+                                            <h3><?php the_title(); ?></h3> 
+                                            <?php the_content(); ?> 
+                                        </div>
+                                    </div>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            <?php else : ?>
+                                <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
+                            <?php endif; ?> 
+                            <nav aria-label="Posts navigation"> 
+                                <?php posts_nav_link( null, __( '&#xAB; Newer Posts', 'blog' ), __( 'Older Posts &#xBB;', 'blog' ) ); ?> 
+                            </nav>                                             
+                        </div>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.', 'blog' ); ?></p>
+                <?php endif; ?> 
             </div>
             <?php if ( get_theme_mod( 'show_right_sidebar' ) ) : ?>
-                <div class="widget-area col-md-3" role="complementary" id="right-sidebar"> 
+                <div class="widget-area col-md-3 single" role="complementary" id="right-sidebar"> 
                     <?php if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
                         <?php dynamic_sidebar( 'right-sidebar' ); ?>
                     <?php endif; ?>                                                                           
