5ee60b46882973acf9651f68d7eebf5e87a601b4
MB <wendigotech@gmail.com>
2018-10-30 15:58:08 +0100

    103

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -23,10 +23,10 @@
                             <?php endif; ?> 
                         </header>
                         <?php the_excerpt( ); ?>
-                        <?php the_excerpt( ); ?>
+                        <hr/>
                         <div class="entry-meta justify-content-between align-items-center"> 
                             <p class="text-center"><?php _e( '&nbsp;by', 'blog' ); ?> <span><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"><?php the_author(); ?></a></span></p>
-                            <a class="btn mt-2 btn-secondary btn-sm" href="<?php echo esc_url( get_permalink() ); ?>"><?php _e( 'Read More', 'blog' ); ?></a> 
+                            <a class="btn mt-2 btn-secondary btn-sm" href="<?php echo esc_url( get_permalink() ); ?>"><?php _e( 'Open', 'blog' ); ?></a> 
                         </div>                         
                     </div>                     
                 </article>
